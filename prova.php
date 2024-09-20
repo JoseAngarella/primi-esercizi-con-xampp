836511cf71a07719e2cd4de785db2d5752e46732
Jose Angarella Crispo <lorebencini13@outlook.com>
2024-09-20 08:34:57 +0200

    primo esercizio finito e funzionante

---
+++ prova.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome="Jose";
+        $cognome="Angarella_Crispo";
+        $peso=75;
+        $tasso_idratazione= $peso*30;
+
+        echo "<h1> $nome $cognome </h1>";
+        echo "<p> il tasso di idratazione di una persona che pesa $peso e' $tasso_idratazione ml </p>";
+
+        
+
+    ?>
+</body>
+</html>
