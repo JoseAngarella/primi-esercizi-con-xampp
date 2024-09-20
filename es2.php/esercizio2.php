1faccaeef4a63a0a10f4f71140b6075206b0fad6
Jose Angarella Crispo <lorebencini13@outlook.com>
2024-09-20 09:02:30 +0200

    esercizio 2 finito e funzionante

---
+++ es2.php/esercizio2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <?php
+        //valori delle variabili modificabile (io li ho messi casualmente)
+        $numgiorni=10;
+        $capitale=1000;
+        $tasso=5; //percentuale    
+
+        //formula per calcolare il tasso di interesse in base ai valori impostati alle variabili
+        $tasso_interesse=($tasso*$numgiorni*$capitale)/36500; 
+
+        echo "<p>tasso: $tasso</p>";
+        echo "<p>capitale: $capitale</p>";
+        echo "<p>numero di giorni: $numgiorni</p>";
+        echo "<h2 style='color:green'>tasso di interesse calcolato: $tasso_interesse</h2>";
+
+    ?>
+    
+</body>
+</html>
